d5d12281c95ecf875e99f774fd6b0abcae2401c0
Shabbirsheikh <41716588+Shabbirsheikh@users.noreply.github.com>
2021-06-12 23:50:19 +0530

    Update sum_of_fractions.c

--- sum_of_fractions.c
+++ sum_of_fractions.c
@@ -10,7 +10,7 @@ int hcf(int a,int b){
 }
 int main(){
 	int a,b,x,y;
-	scanf("%d %d %d %d",&x,&a,&y,&b);
+	scanf("%d %d %d %d",&x,&a,&y,&b);//As 1st fraction is x/a and 2nd fraction is y/b//
 	int m,n;
 	int lcm;
 	int i;
@@ -19,8 +19,8 @@ int main(){
 	lcm=(a*b)/hcf(a,b);
 	m=(lcm/a)*x+(lcm/b)*y;
 	int p,q;
-	p=m/hcf(lcm,m);
+	p=m/hcf(lcm,m);//it may be possible that m,lcm i.e., desired numerator and denominator may have common multiple so we have to remove them//
 	q=lcm/hcf(lcm,m);
-	printf("%d/%d+%d/%d=%d/%d",x,a,y,b,p,q);
+	printf("%d/%d+%d/%d=%d/%d",x,a,y,b,p,q);//x/a+y/b=p/q//
 	return 0;
 }
